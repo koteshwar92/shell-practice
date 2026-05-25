e6f2e3ec76f6c9121d995e85be5c7868c963a1c8
Koteshwar92 <koteshwarkrishitha21@gmail.com>
2026-05-25 12:36:58 +0000

    actions

--- Varables.sh
+++ Varables.sh
@@ -4,11 +4,11 @@ Person1=Kotesh
 
 Person2=Priya
 
-echo $Person1 = Hi , How are you ?
+echo $Person1 = Hi $Person2, How are you ?
 
-echo $Person2 = fine , what about you .
+echo $Person2 = fine $Person1, what about you .
 
-echo $Person1 = which village .
+echo $Person1 = $Person2 are you from which village .
 
  
 
