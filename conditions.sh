9cd25289d07d91f946e1683ee0aef388b1217ccb
Koteshwar92 <koteshwarkrishitha21@gmail.com>
2026-05-27 04:43:58 +0000

    datatypes done

--- conditions.sh
+++ conditions.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
-Number=100
+Number=20
 
 if [ $Number -gt 20 ];then
 
 echo "given number $Number is greater than 20"
+    
+elif [ $Number -lt 20 ];then
+
+echo "given number $Number is less than 20"
+
+else 
+
+echo "given number $Number is equal to 20"
 
 fi
-
 
